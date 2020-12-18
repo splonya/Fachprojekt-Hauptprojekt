04646807b844b5a8b695d96b62ebe676a8a4e9b3
Kostiantyn Sploshnov <sploshnov123@gmail.com>
2020-12-18 15:13:10 +0100

    added spawners und collisions for projectiles

--- Assets/Scripts/CustomNetworkManager.cs
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -8,9 +8,12 @@ public class CustomNetworkManager : NetworkManager
     public Transform firstPosition;
     public Transform secondPosition;
 
-    public Transform TESTpos;
+    public float projectileVelocity = 1;
+
     GameObject ball;
 
+    public Transform[] spawners;
+    
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         var positionToSpawn = numPlayers == 0 ? firstPosition : secondPosition;
@@ -25,8 +28,32 @@ public class CustomNetworkManager : NetworkManager
 
         if (numPlayers == 1)
         {
-            ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Square"));
-            NetworkServer.Spawn(ball);
+            //ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Projectile"));
+            //NetworkServer.Spawn(ball);
+
+            StartCoroutine(onCoroutine());
+        }
+    }
+
+    IEnumerator onCoroutine()
+    {
+        while (true)
+        {
+            var rnd = new System.Random();
+            var spawnId = rnd.Next(0, spawners.Length);
+            var directionSpawnId = (spawnId + spawners.Length / 2) % spawners.Length;
+
+            Debug.LogError(spawners[spawnId].position);
+
+            var projectile = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Projectile"), new Vector3(spawners[spawnId].position.x, spawners[spawnId].position.y, 0), Quaternion.identity);
+
+            projectile.GetComponent<Rigidbody2D>().velocity = -Vector3.MoveTowards(spawners[spawnId].position, spawners[directionSpawnId].position, projectileVelocity * Time.deltaTime);
+
+            Debug.Log($"start: {spawnId}, target: {directionSpawnId}");
+
+            NetworkServer.Spawn(projectile);
+
+            yield return new WaitForSeconds(5f);
         }
     }
 }
